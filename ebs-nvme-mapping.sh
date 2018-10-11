55473a7b0dcea10ab16bca865a676e90e4275311
Victor Chavez <vdczzz@users.noreply.github.com>
2018-10-11 15:37:01 -0700

    Update ebs-nvme-mapping.sh
    - nvme cli doesn't return the leading `/dev/`, so add it
    - `/dev/sda1` isn't a disk, it's a partition, so map accordingly

--- ebs-nvme-mapping.sh
+++ ebs-nvme-mapping.sh
@@ -3,8 +3,14 @@
 PATH="${PATH}:/usr/sbin"
 
 for blkdev in $( nvme list | awk '/^\/dev/ { print $1 }' ) ; do
-  mapping=$(nvme id-ctrl --raw-binary "${blkdev}" | cut -c3073-3104 | tr -s ' ' | sed 's/ $//g')
-  if [[ "${mapping}" == /dev/* ]]; then
+  mapping='/dev/'"$(nvme id-ctrl --raw-binary "${blkdev}" | cut -c3073-3104 | sed 's/\s*$//')"
+  if [[ "${mapping}" == "/dev/sda1" ]]; then
+    echo "${mapping}"
+    echo "if passed"
+    ( test -b "${blkdev}p1" && test -L "${mapping}" ) || ln -s "${blkdev}p1" "${mapping}"
+  elif [[ "${mapping}" == /dev/* ]]; then
+    echo "${mapping}"
+    echo "if passed"
     ( test -b "${blkdev}" && test -L "${mapping}" ) || ln -s "${blkdev}" "${mapping}"
   fi
 done
